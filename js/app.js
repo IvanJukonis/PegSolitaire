7cd1ec298b2896b7463f67155eee6ace8d86c92c
Ivan.Jukonis <IvanJukonis@hotmail.com>
2019-07-18 15:56:20 -0300

    PS: Project initial structure

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var board = [
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 0 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined]
+  ]
