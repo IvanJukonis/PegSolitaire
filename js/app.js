55635eac3ab4e94d7db1f6a38e452abde88c9cea
Ivan.Jukonis <IvanJukonis@hotmail.com>
2019-07-22 14:49:59 -0300

    PS: Css update

--- js/app.js
+++ js/app.js
@@ -1,58 +1,64 @@
 var board = [
-    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
-    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
-    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
-    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 0 }, { value: 1 }, { value: 1 }, { value: 1 }],
-    [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
-    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
-    [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined]
+  [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+  [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+  [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+  [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 0 }, { value: 1 }, { value: 1 }, { value: 1 }],
+  [{ value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }, { value: 1 }],
+  [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined],
+  [undefined, undefined, { value: 1 }, { value: 1 }, { value: 1 }, undefined, undefined]
 ]
+
+//#region Generate Board
 
 //Returns string with row number and column number (used to make id)
 var createId = function (rowN, colN) {
-    return 'ball-' + rowN + '-' + colN
+  return 'ball-' + rowN + '-' + colN
 }
 
 //Create buttons inside a row with an id and a class
 var generateCell = function (cell, rowN, colN) {
-    var html = '<button id="' + createId(rowN, colN) + '" class="'
-    if (cell && cell.value) {
-      html += 'ball'
-    }
-    else if (cell && cell.value == 0) {
-      html += 'empty'
-    }
-    else {
-      html += 'hidden'
-    }
-    html += '"></button>'
-    return html
+  var html = '<button id="' + createId(rowN, colN) + '" class="'
+  if (cell && cell.value) {
+    html += 'ball'
   }
+  else if (cell && cell.value == 0) {
+    html += 'empty'
+  }
+  else {
+    html += 'hidden'
+  }
+  html += '"></button>'
+  return html
+}
 
   //Creates label "row" and generates cells dependig on his lenght (board[i] lenght)
-  var generateRow = function (row, rowN) {
-    var html = '<div class="row">'
-    for (let j = 0; j < row.length; j++) {
-      html += generateCell(row[j], rowN, j)
-    }
-    html += '</div>'
-    return html
+var generateRow = function (row, rowN) {
+  var html = '<div class="row">'
+  for (let j = 0; j < row.length; j++) {
+    html += generateCell(row[j], rowN, j)
   }
+  html += '</div>'
+  return html
+}
   
   //Creates label "game" and executes "generateRow" depending on board lenght (0-6)
-  var generateBoard = function () {
-    var html = '<div class="game">'
-    for (i = 0; i < board.length; i++) {
-      html += generateRow(board[i], i)
-    }
-    html += '</div>'
-    return html
+var generateBoard = function () {
+  var html = '<div class="game">'
+  for (i = 0; i < board.length; i++) {
+    html += generateRow(board[i], i)
   }
+  html += '</div>'
+  return html
+}
 
-  //Init Function
-  var init = function () {
-    var boardElement = document.getElementById('board')
-    boardElement.innerHTML = generateBoard()
-  }
+//#endregion
+
+//Init Function
+var init = function () {
+  var boardElement = document.getElementById('board')
+  boardElement.innerHTML = generateBoard()
+  var Balls = boardElement.getElementsByClassName('ball')
+  addBallsEventHandlers(Balls)
+}
   
-  window.onload = init
+window.onload = init
