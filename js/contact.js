5abe46f3d1e0a9e0f5b4a8e8918dc05e95d6594d
Ivan.Jukonis <IvanJukonis@hotmail.com>
2019-08-07 13:55:48 -0300

    PS: Contact JS buttons handlers

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,31 @@
+var sendEmail = function() {
+    
+}
 
 var returnIndex = function() {
     window.location = 'index.html'
 }
 
-var returnIndexAddEventHandler = function (returnBtn) {
-    returnBtn.onclick = returnIndex
+var addFormButtonsEventHandlers = function (formBtn) {
+    for (let i = 0; i < formBtn.length; i++) {
+        formBtn[i].onclick = pressButtonForm
+    }
+  }
+  
+var pressButtonForm = function (evt) {
+    var id = evt.target.id
+    if (id == 'return-index-btn') {
+      returnIndex()
+    }
+    else if (id == 'send-form-btn') {
+      sendEmail()
+    }
 }
 
 
 var init = function() {
-    returnBtn = document.getElementById('return-index')
-    returnIndexAddEventHandler(returnBtn)
+    formBtn = document.getElementById('form-btn')
+    addFormButtonsEventHandlers(formBtn)
 }
 
 window.onload = init
